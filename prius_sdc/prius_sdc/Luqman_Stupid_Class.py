16994ec0184e0aa9033400e1325134b0266e0b90
noshluk2 <noshluk2@gmail.com>
2021-08-16 01:04:52 +0500

    Node Video Feed in created , Python modules not added

--- prius_sdc/prius_sdc/Luqman_Stupid_Class.py
+++ prius_sdc/prius_sdc/Luqman_Stupid_Class.py
@@ -1,99 +1,68 @@
 import cv2
 import numpy as np
 import os
-import config
 
-# ****************************************************  DETECTION ****************************************************
-# ****************************************************    LANES   ****************************************************
-from Detection.Lanes.Lane_Detection import Detect_Lane
-# ****************************************************    SIGNS   ****************************************************
-from Detection.Signs.SignDetectionApi import detect_Signs
-
-
-# ****************************************************   CONTROL  *****************************************************
-from Control.special import Drive_Car
-#from Control.Drive import Drive_Car
+from .Detection.Lanes.Lane_Detection import Detect_Lane
+from .Detection.Signs.SignDetectionApi import detect_Signs
+from .Control.special import Drive_Car
+from .config import config
+from rclpy.node import Node 
+from cv_bridge import CvBridge 
+from sensor_msgs.msg import Image 
+import rclpy 
 
 
-Live_Testing = False
 
-def main():
+class Video_feed_in(Node):
+    def __init__(self):
+        super().__init__('video_subscriber')
+        self.subscriber = self.create_subscription(Image,'/camera1/image_raw',self.process_data,10)
+        self.bridge = CvBridge() # converting ros images to opencv data
+        self.self.prev_Mode = "Detection"
+        self.self.Left_turn_iterations = 0
+        self.self.Frozen_Curvature=0
+        self.self.Frozen_Curvature=0
 
-    if Live_Testing:
-        cap = cv2.VideoCapture(0)
-    else:
-        #cap = cv2.VideoCapture(os.path.abspath("data/vids/Ros2/signs_and_road.avi"))#720p
-        #cap = cv2.VideoCapture(os.path.abspath("data/vids/Ros2/signs_training.avi"))#720p
-        #cap = cv2.VideoCapture(os.path.abspath("data/vids/Ros2/full_track_drive.avi"))#720p
-        cap = cv2.VideoCapture(os.path.abspath("data/vids/Ros2/ishara+turning.avi"))#720p
-        #cap = cv2.VideoCapture(os.path.abspath("data/vids/Ros2/NEW_full_track_tour.mp4"))#720p
-
-    waitTime = 0
-    
-    prev_Mode = "Detection"
-    Left_turn_iterations = 0
-
-    while(1):
-
-        ret,img = cap.read()
-        if not ret:
-            break
-
-        img = img[0:640,402:1267] # Cropping for 720p Less Skew Righty
-
-
-        # >>>>>>>>>>>>>>>>>>>>>>>> Optimization No 1 [RESIZING]<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+        
+    def process_data(self, data): 
+        frame = self.bridge.imgmsg_to_cv2(data) # performing conversion
+        img = frame[0:640,402:1267] 
         img = cv2.resize(img,(320,240))
-        img_orig = img.copy()# Keep it for
-
-        # ****************************************************  DETECTION [LANES] ****************************************************
-        # 1. Extracting Information that best defines relation between lanes and Car
+        img_orig = img.copy()
         distance, Curvature = Detect_Lane(img)
-        # ****************************************************  DETECTION  [LANES] ****************************************************
-
-        # ****************************************************  DETECTION  [SIGNS] ****************************************************
-        # 2. Detecting and extracting information from the signs
         Mode , Tracked_class = detect_Signs(img_orig,img)
-        # ****************************************************  DETECTION  [SIGNS] ****************************************************
-
-        # ****************************************************  DETECTION  [LeftHandTurn] ****************************************************
-        if ( (prev_Mode =="Detection") and (Mode=="Tracking") and (Tracked_class=="left_turn") ):
-            prev_Mode = "Tracking"
+        if ( (self.prev_Mode =="Detection") and (Mode=="Tracking") and (Tracked_class=="left_turn") ):
+            self.prev_Mode = "Tracking"
             config.Activat_LeftTurn = True
-            Frozen_Dist = distance
-            Frozen_Curvature = Curvature
+            self.Frozen_Curvature = distance
+            self.Frozen_Curvature = Curvature
             print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Tracking <<<<<<<<<<<<<<<<<<<<<<<<<<<")
-        elif ( (prev_Mode =="Tracking") and (Mode=="Detection") and (Tracked_class=="left_turn") ):
-            # Car just besides left turn,
-            # Keep moving left 60 degrees for 100 iterations 
+        elif ( (self.prev_Mode =="Tracking") and (Mode=="Detection") and (Tracked_class=="left_turn") ):
             print("Left Activated")
             print("config.Activat_LeftTurn ",config.Activat_LeftTurn)
-            if ((Left_turn_iterations % 2 ) ==0):
-                Frozen_Curvature = Frozen_Curvature -1 # Move left by 1 degree 
-            if(Left_turn_iterations==100):
+            if ((self.Left_turn_iterations % 2 ) ==0):
+                self.Frozen_Curvature = self.Frozen_Curvature -1 # Move left by 1 degree 
+            if(self.Left_turn_iterations==100):
                 print("Left DeActivated")
-                # Dectivate Left turn by Setting prev_Mode = "Detection"
-                prev_Mode = "Detection"
+                self.prev_Mode = "Detection"
                 config.Activat_LeftTurn = False
-                Left_turn_iterations = 0
-            Left_turn_iterations = Left_turn_iterations + 1
-        # ****************************************************  DETECTION  [LeftHandTurn] ****************************************************
-
-        # ****************************************************  CONTROL ****************************************************
-        # 3. Drive Car On Basis of Current State Info provided by Detection module
-        if (config.Activat_LeftTurn == True):
-            distance = Frozen_Dist
-            Curvature = Frozen_Curvature
+                self.Left_turn_iterations = 0
+            self.Left_turn_iterations = self.Left_turn_iterations + 1
+            if (config.Activat_LeftTurn == True):
+                distance = self.Frozen_Curvature
+                Curvature = self.Frozen_Curvature
         Current_State = [distance, Curvature , img , Mode , Tracked_class]
         Drive_Car(Current_State)
-        # ****************************************************  CONTROL ****************************************************
         
         cv2.imshow("Frame",img)
-        k = cv2.waitKey(waitTime)
-        if k==27:
-            break
+        cv2.waitKey(1)
+        
  
-
+def main(args=None):
+  rclpy.init(args=args)
+  image_subscriber = Video_feed_in()
+  rclpy.spin(image_subscriber)
+  rclpy.shutdown()
 
 if __name__ == '__main__':
 	main()
--- prius_sdc/setup.py
+++ prius_sdc/setup.py
@@ -1,12 +1,24 @@
 from setuptools import setup
 import os
 from glob import glob
+
+from setuptools import find_packages
+
+# packages=find_packages(exclude=['Detection'])
+
+
 package_name = 'prius_sdc'
+detection_L_module ="prius_sdc/Detection/Lanes"
+detection_S_module ="prius_sdc/Detection/Signs"
+config_module = "prius_sdc/config" 
+data_module ="prius_sdc/data"
+Control_module ="prius_sdc/Control"
+
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=[package_name],
+    packages=['prius_sdc',Control_module,data_module,detection_S_module,detection_L_module,config_module],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
@@ -22,6 +34,8 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+                    'sdc = prius_sdc.Luqman_Stupid_Class:main',
+
         ],
     },
 )
