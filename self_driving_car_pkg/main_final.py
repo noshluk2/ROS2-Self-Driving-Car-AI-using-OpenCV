ca1e2380b2dea32c074dfcbf35ca59d4dd2defef
HaiderAbasi <haiderabbasi333@gmail.com>
2021-08-19 07:53:33 +0500

    Tried Testing locally in Main_final TLD Testing in MAin mini (Found False Detections)

--- self_driving_car_pkg/main_final.py
+++ self_driving_car_pkg/main_final.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+from numpy import interp
 import os
 import config
 
@@ -9,6 +10,7 @@ from Detection.Lanes.Lane_Detection import Detect_Lane
 # ****************************************************    SIGNS   ****************************************************
 from Detection.Signs.SignDetectionApi import detect_Signs
 
+from .Detection.Signs.a_Localization.TLD import detect_TrafficLight
 
 # ****************************************************   CONTROL  *****************************************************
 from Control.special import Drive_Car
@@ -22,11 +24,8 @@ def main():
     if Live_Testing:
         cap = cv2.VideoCapture(0)
     else:
-        #cap = cv2.VideoCapture(os.path.abspath("data/vids/Ros2/signs_and_road.avi"))#720p
-        #cap = cv2.VideoCapture(os.path.abspath("data/vids/Ros2/signs_training.avi"))#720p
-        #cap = cv2.VideoCapture(os.path.abspath("data/vids/Ros2/full_track_drive.avi"))#720p
-        cap = cv2.VideoCapture(os.path.abspath("data/vids/Ros2/ishara+turning.avi"))#720p
-        #cap = cv2.VideoCapture(os.path.abspath("data/vids/Ros2/NEW_full_track_tour.mp4"))#720p
+        vid_Path=os.path.abspath("self_driving_car_pkg/data/vids/ishara_turning.avi").replace('\\', '/')
+        cap = cv2.VideoCapture(vid_Path)
 
     waitTime = 0
     
@@ -35,63 +34,55 @@ def main():
 
     while(1):
 
-        ret,img = cap.read()
+        ret,frame = cap.read()
         if not ret:
             break
 
-        img = img[0:640,402:1267] # Cropping for 720p Less Skew Righty
-
-
-        # >>>>>>>>>>>>>>>>>>>>>>>> Optimization No 1 [RESIZING]<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+        img = frame[0:640,238:1042] 
         img = cv2.resize(img,(320,240))
-        img_orig = img.copy()# Keep it for
+        img_orig = img.copy()
+        frame_draw = img.copy()
+        Traffic_State = detect_TrafficLight(img,frame_draw)
 
-        # ****************************************************  DETECTION [LANES] ****************************************************
-        # 1. Extracting Information that best defines relation between lanes and Car
         distance, Curvature = Detect_Lane(img)
-        # ****************************************************  DETECTION  [LANES] ****************************************************
-
-        # ****************************************************  DETECTION  [SIGNS] ****************************************************
-        # 2. Detecting and extracting information from the signs
         Mode , Tracked_class = detect_Signs(img_orig,img)
-        # ****************************************************  DETECTION  [SIGNS] ****************************************************
-
-        # ****************************************************  DETECTION  [LeftHandTurn] ****************************************************
         if ( (prev_Mode =="Detection") and (Mode=="Tracking") and (Tracked_class=="left_turn") ):
             prev_Mode = "Tracking"
             config.Activat_LeftTurn = True
-            Frozen_Dist = distance
+            Frozen_Curvature = distance
             Frozen_Curvature = Curvature
             print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Tracking <<<<<<<<<<<<<<<<<<<<<<<<<<<")
         elif ( (prev_Mode =="Tracking") and (Mode=="Detection") and (Tracked_class=="left_turn") ):
-            # Car just besides left turn,
-            # Keep moving left 60 degrees for 100 iterations 
             print("Left Activated")
             print("config.Activat_LeftTurn ",config.Activat_LeftTurn)
-            if ((Left_turn_iterations % 2 ) ==0):
+            if ( ((Left_turn_iterations % 24 ) ==0) and (Left_turn_iterations>25) ):
                 Frozen_Curvature = Frozen_Curvature -1 # Move left by 1 degree 
             if(Left_turn_iterations==100):
                 print("Left DeActivated")
-                # Dectivate Left turn by Setting prev_Mode = "Detection"
                 prev_Mode = "Detection"
                 config.Activat_LeftTurn = False
                 Left_turn_iterations = 0
             Left_turn_iterations = Left_turn_iterations + 1
-        # ****************************************************  DETECTION  [LeftHandTurn] ****************************************************
-
-        # ****************************************************  CONTROL ****************************************************
-        # 3. Drive Car On Basis of Current State Info provided by Detection module
-        if (config.Activat_LeftTurn == True):
-            distance = Frozen_Dist
-            Curvature = Frozen_Curvature
+            if (config.Activat_LeftTurn == True):
+                distance = Frozen_Curvature
+                Curvature = Frozen_Curvature
         Current_State = [distance, Curvature , img , Mode , Tracked_class]
-        Drive_Car(Current_State)
-        # ****************************************************  CONTROL ****************************************************
-        
+        a,b=Drive_Car(Current_State)
+        a=interp(a,[30,120],[0.5,-0.5])
+        b=interp(b,[50,90],[1,2])
+        print("\n\nComputed A = ",a,"   B = ", b,"\n\n")
+        a=a
+
+        if(Traffic_State == "Stop"):
+            b = 0.0 # Noob luqman
+        else:
+            b = b
+        print("\n\nAfter Traffic Light A = ",a,"   B = ", b,"\n\n")
+
+
+        cv2.putText(img,Traffic_State,(20,20),cv2.FONT_HERSHEY_COMPLEX,2,255)
         cv2.imshow("Frame",img)
-        k = cv2.waitKey(waitTime)
-        if k==27:
-            break
+        cv2.waitKey(1)
  
 
 
--- self_driving_car_pkg/main_mini.py
+++ self_driving_car_pkg/main_mini.py
@@ -1,11 +1,12 @@
 import cv2
 import os
-from Detection.Signs.a_Localization.UsingHough_2 import detect_TrafficLight
+from Detection.Signs.a_Localization.TLD import detect_TrafficLight
 from Detection.Signs.a_Localization.UsingHough import detect_Circles
 
 def main():
     #cap = cv2.VideoCapture(os.path.abspath("data/vids/Ros2/NEW_full_track_tour.mp4"))
-    cap = cv2.VideoCapture(os.path.abspath("data/vids/Ros2/ishara+turning.avi"))
+    vid_Path=os.path.abspath("self_driving_car_pkg/data/vids/ishara_turning.avi").replace('\\', '/')
+    cap = cv2.VideoCapture(vid_Path)
     waitTime = 0
     while(1):
         ret,img = cap.read()
