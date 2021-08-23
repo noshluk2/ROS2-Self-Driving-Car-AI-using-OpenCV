cc9f1411785c0d65cbf0ce0e01d5ad9bac5dca4a
HaiderAbasi <haiderabbasi333@gmail.com>
2021-08-23 19:23:21 +0500

    Choose better nameConvention + Moved ObeyLeftTurn To Control Class

--- self_driving_car_pkg/self_driving_car_pkg/computer_vision_node.py
+++ self_driving_car_pkg/self_driving_car_pkg/computer_vision_node.py
@@ -3,12 +3,12 @@ from numpy import interp
 import numpy as np
 
 from .config import config
-from .Detection.Lanes.Lane_Detection import Detect_Lane
+from .Detection.Lanes.Lane_Detection import detect_Lane
 from .Detection.Signs.SignDetectionApi import detect_Signs
 # from .Detection.Signs.a_Localization.TLD import detect_TrafficLight
-from .Detection.Signs.a_Localization.ObjDet_cascade_classified import Extract_TL_With_State
+from .Detection.Signs.a_Localization.ObjDet_cascade_classified import detect_TrafficLights
 from .Control.special import Drive_Car
-from .Control.Control_TrafficL_Nd_LeftTurn import TL_Control
+from .Control.Control_TrafficL_Nd_LeftTurn import Control
 
 from geometry_msgs.msg import Twist
 from rclpy.node import Node 
@@ -17,7 +17,7 @@ from sensor_msgs.msg import Image
 import rclpy 
 
 
-TL_control = TL_Control() 
+control = Control() 
 class Video_feed_in(Node):
     def __init__(self):
 
@@ -28,52 +28,34 @@ class Video_feed_in(Node):
 
         self.velocity=Twist()
         self.bridge = CvBridge() # converting ros images to opencv data
-        self.prev_Mode = "Detection"
+        
 
-        self.Left_turn_iterations = 0
-        self.Frozen_Curvature=0
 
     def send_cmd_vel(self):
         self.publisher.publish(self.velocity)
         
     def process_data(self, data): 
+        
         frame = self.bridge.imgmsg_to_cv2(data,'bgr8') # performing conversion
         img = frame[0:640,238:1042] 
         img = cv2.resize(img,(320,240))
         img_orig = img.copy()
         #Traffic_State = detect_TrafficLight(img,frame_draw)
-        Traffic_State, CloseProximity = Extract_TL_With_State(img_orig.copy())
+        Traffic_State, CloseProximity = detect_TrafficLights(img_orig.copy())
 
-        distance, Curvature = Detect_Lane(img)
+        distance, Curvature = detect_Lane(img)
         Mode , Tracked_class = detect_Signs(img_orig,img)
-        if ( (self.prev_Mode =="Detection") and (Mode=="Tracking") and (Tracked_class=="left_turn") ):
-            self.prev_Mode = "Tracking"
-            config.Activat_LeftTurn = True
-            self.Frozen_Curvature = distance
-            self.Frozen_Curvature = Curvature
-            print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Tracking <<<<<<<<<<<<<<<<<<<<<<<<<<<")
-        elif ( (self.prev_Mode =="Tracking") and (Mode=="Detection") and (Tracked_class=="left_turn") ):
-            print("Left Activated")
-            print("config.Activat_LeftTurn ",config.Activat_LeftTurn)
-            if ( ((self.Left_turn_iterations % 24 ) ==0) and (self.Left_turn_iterations>25) ):
-                self.Frozen_Curvature = self.Frozen_Curvature -1 # Move left by 1 degree 
-            if(self.Left_turn_iterations==100):
-                print("Left DeActivated")
-                self.prev_Mode = "Detection"
-                config.Activat_LeftTurn = False
-                self.Left_turn_iterations = 0
-            self.Left_turn_iterations = self.Left_turn_iterations + 1
-            if (config.Activat_LeftTurn == True):
-                distance = self.Frozen_Curvature
-                Curvature = self.Frozen_Curvature
+
+        distance,Curvature = control.Obey_LeftTurn(distance,Curvature,Mode,Tracked_class)
+
         Current_State = [distance, Curvature , img , Mode , Tracked_class]
-        a,b=Drive_Car(Current_State)
+        a,b = Drive_Car(Current_State)
+
         a=interp(a,[30,120],[0.5,-0.5])
         b=interp(b,[50,90],[1,2])
         print("\n\nA = ",a,"   B = ", b,"\n\n")
-        #self.velocity.angular.z = a
 
-        a,b = TL_control.OBEY_TrafficLights(a,b,Traffic_State,CloseProximity)
+        a,b = control.OBEY_TrafficLights(a,b,Traffic_State,CloseProximity)
         print("After Obeying TrafficLights")
         print("\n\nA = ",a,"   B = ", b,"\n\n")
 
