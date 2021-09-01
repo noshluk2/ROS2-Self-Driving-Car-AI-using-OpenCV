Corrected Left Turn Range issues + Removed Unneccesary prints + Loosened Circle detection criteria for Stop SIgn Detection

self_driving_car_pkg/self_driving_car_pkg/Detection/Signs/SignDetectionApi.py
@@ -79,14 +79,14 @@ def SignDetection_Nd_Tracking(gray,cimg,frame_draw,model):
     # 3. IF Mode of SignTrack is Detection , Proceed
     if (signTrack.mode == "Detection"):
         # cv2.putText(frame_draw,"Sign Detected ==> "+str(signTrack.Tracked_class),(20,85),cv2.FONT_HERSHEY_COMPLEX,0.75,(255,255,0),2)
-        NumOfVotesForCircle = 50 #parameter 1 MinVotes needed to be classified as circle
+        NumOfVotesForCircle = 30 #parameter 1 MinVotes needed to be classified as circle
         CannyHighthresh = 250 # High threshold value for applying canny
         mindDistanBtwnCircles = 100 # kept as sign will likely not be overlapping
         max_rad = 140 # smaller circles dont have enough votes so only maxRadius need to be controlled 
                         # As signs are right besides road so they will eventually be in view so ignore circles larger than said limit
 
         # 4. Detection (Localization)
-        circles = cv2.HoughCircles(gray,cv2.HOUGH_GRADIENT,1,mindDistanBtwnCircles,param1=CannyHighthresh,param2=NumOfVotesForCircle,minRadius=10,maxRadius=max_rad)
+        circles = cv2.HoughCircles(gray,cv2.HOUGH_GRADIENT,1,mindDistanBtwnCircles,param1=CannyHighthresh,param2=NumOfVotesForCircle,minRadius=18,maxRadius=max_rad)
 
         # 4a. Detection (Localization) Checking if circular regions were localized
         if circles is not None:
@@ -180,7 +180,8 @@ def SignDetection_Nd_Tracking(gray,cimg,frame_draw,model):
         p1, st, err = cv2.calcOpticalFlowPyrLK(signTrack.old_gray, gray, signTrack.p0, None,**signTrack.lk_params)
         
         # 5a. If no flow, look for new points
-        if p1 is None:
+        if ( (p1 is None) or ( len(p1[st == 1])<3 ) ):
+        #if p1 is None:
             signTrack.mode = "Detection"
             signTrack.mask = np.zeros_like(frame_draw)
             signTrack.Reset()

self_driving_car_pkg/self_driving_car_pkg/computer_vision_node.py
@@ -88,40 +88,27 @@ class Video_feed_in(Node):
         img_b4_resiz = img.copy()
         img = cv2.resize(img,(320,240))
         img_orig = img.copy()
+
         #Traffic_State = detect_TrafficLight(img,frame_draw)
         Traffic_State, CloseProximity = detect_TrafficLights(img_orig.copy())
 
         distance, Curvature = detect_Lane(img)
         Mode , Tracked_class = detect_Signs(img_orig,img)
-        #Mode , Tracked_class = detect_Signs(img_b4_resiz,img_b4_resiz)
-        #img_a4tr_resiz = cv2.resize(img_b4_resiz,(320,240))
-        #img = cv2.bitwise_or(img_a4tr_resiz,img)
 
         distance,Curvature, Detected_LeftTurn, Activat_LeftTurn = control.Obey_LeftTurn(distance,Curvature,Mode,Tracked_class)
 
         Current_State = [distance, Curvature , img , Mode , Tracked_class]
         a,b = Drive_Car(Current_State)
 
-        #a=interp(a,[30,120],[0.5,-0.5])
-        #b=interp(b,[30,90],[1,2])
-        print("\n\nA = ",a,"   B = ", b,"\n\n")
-
         a,b = control.OBEY_TrafficLights(a,b,Traffic_State,CloseProximity)
-        print("After Obeying TrafficLights")
-        print("\n\nA = ",a,"   B = ", b,"\n\n")
 
-        #self.velocity.linear.x = b        
-        #self.velocity.angular.z = a
-
-        #angle_of_car = interp(a,[0.5,-0.5],[-45,45])
-        #current_speed = interp(b,[1,2],[30,90])
         angle_of_car = interp(a,[30,120],[-45,45])
         current_speed = b
+
         angle_speed_str = "[ Angle ,Speed ] = [ " + str(int(angle_of_car)) + "deg ," + str(int(current_speed)) + "mph ]"
         cv2.putText(img,str(angle_speed_str),(20,20),cv2.FONT_HERSHEY_DUPLEX,0.4,(0,0,255),1)
 
         cv2.putText(img,"Traffic Light State = [ "+Traffic_State+" ] ",(20,60),cv2.FONT_HERSHEY_COMPLEX,0.35,255)
-        #cv2.putText(img,"Sign Detected ==> "+str(Tracked_class),(20,85),cv2.FONT_HERSHEY_COMPLEX,0.35,(255,0,0),1)
         
         if (Tracked_class=="left_turn"):
             font_Scale = 0.30
@@ -133,10 +120,10 @@ class Video_feed_in(Node):
             font_Scale = 0.35
         cv2.putText(img,"Sign Detected ==> "+str(Tracked_class),(20,85),cv2.FONT_HERSHEY_COMPLEX,font_Scale,(255,0,0),1)
 
-        #cv2.putText(img,"Angle = "+str(np.round(a,3))+" , Speed = " + str(np.round(b,3)) ,(20,80),cv2.FONT_HERSHEY_COMPLEX,0.5,255)
         a=interp(a,[30,120],[0.5,-0.5])
-        b=interp(b,[30,90],[1,2])
-        self.velocity.linear.x = b        
+        if (b!=0):
+            b=interp(b,[30,90],[1,2])
+        self.velocity.linear.x = float(b)        
         self.velocity.angular.z = a
 
         cv2.imshow("Frame",img)