76dd847fc08141609bfa0621dfa6d11fe4c1dad5
HaiderAbasi <haiderabbasi333@gmail.com>
2022-06-24 01:07:26 +0500

    Added new world (maze_solving) -> worlds/. [ Required for Implementing and testing Sat-Nav(Coming Soon!) ;) ]

---
+++ self_driving_car_pkg/scripts/lights_spawner_maze.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+### This bash is going to spawn Traffic Lights into your simulation at origin 
+### Inside the traffic stand
+green_light_sdf=$HOME/.gazebo/models/green_light/model.sdf
+red_light_sdf=$HOME/.gazebo/models/red_light/model.sdf
+yellow_light_sdf=$HOME/.gazebo/models/yellow_light/model.sdf
+
+
+ros2 run self_driving_car_pkg spawner_node $red_light_sdf red_light 14.72 4.26 
+sleep 7.5
+ros2 run self_driving_car_pkg spawner_node $yellow_light_sdf yellow_light 13.7 4.26
+sleep 1
+ros2 run self_driving_car_pkg spawner_node $green_light_sdf green_Light 12.66 4.26
